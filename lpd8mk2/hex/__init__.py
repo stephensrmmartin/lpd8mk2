6859de99e2d2a11ea97a136f4f2e5adf734b2797
Stephen Martin <stephensrmmartin@gmail.com>
2024-05-06 19:34:53 -0700

    Added rgb and basic helper

---
+++ lpd8mk2/hex/__init__.py
@@ -0,0 +1,4 @@
+
+from .hex import Hexcode
+
+from .util import hex_to_int, split_byte_into_two
---
+++ lpd8mk2/rgb/rgb.py
@@ -0,0 +1,6 @@
+from ..hex import *
+
+
+def color_code_to_hexcodes(x: int) -> list[Hexcode]:
+    return split_byte_into_two(x)
+
